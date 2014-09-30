9a8a3d736cdea7a26fb66743b9a8b9d78a4b608b
Suman Majumdar <mid.suman@gmail.com>
2014-09-30 17:05:51 +0200

    Removed some definition.

--- ion.h
+++ ion.h
@@ -5,7 +5,7 @@ fftw_real  ***allocate_fftw_real_3d(int N1,int N2,int N3);
 /* in ionz_funcs.c */
 void Setting_Up_Memory_For_ionz(int Nnion);
 void smooth(fftw_real ***ro_dum,float Radii);
-void smooth_k_sharp(fftw_real ***ro_dum,float Radii);              
+     
 
 
 
