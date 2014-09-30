842b139eeda37f7fc5f2a4dff722559e75daef55
Suman Majumdar <mid.suman@gmail.com>
2014-09-30 16:19:21 +0200

    Added small comments.

--- ion.h
+++ ion.h
@@ -1,8 +1,9 @@
 /* in allotarrays.c */
 float **allocate_float_2d(long N1,int N2);
 fftw_real  ***allocate_fftw_real_3d(int N1,int N2,int N3);
-void Setting_Up_Memory_For_ionz(int Nnion);
 
+/* in ionz_funcs.c */
+void Setting_Up_Memory_For_ionz(int Nnion);
 void smooth(fftw_real ***ro_dum,float Radii);
 void smooth_k_sharp(fftw_real ***ro_dum,float Radii);              
 
