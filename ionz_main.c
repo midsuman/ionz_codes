f6251fcf2cb8e46d847838929d0bd2e88ee0f542
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-15 13:35:33 +0100

    quick commit

--- ionz_main.c
+++ ionz_main.c
@@ -200,7 +200,7 @@ int main(int argc, char **argv) {
   MPI_Barrier(MPI_COMM_WORLD);
   MPI_Bcast(&robarhalo, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 #endif
-
+  if(mympi.ThisTask == 0) debug_checkpoint();
   unpack_3d_array_mpi_transfer(buffer,ngamma,N1,N2,N3);
   free(buffer);
 
