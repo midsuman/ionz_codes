491a41f11dba914781604d03e3338272b542588e
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-15 13:49:48 +0100

    quick commit

--- ionz_main.c
+++ ionz_main.c
@@ -162,8 +162,9 @@ int main(int argc, char **argv) {
   /* Allocating memory to different arrays */
   // Setting_Up_Memory_For_ionz(Nnion, N1, N2, N3
   nh = allocate_fftw_real_3d(N1,N2,N3+2);
-  if(mympi.ThisTask == 0) printf("%f\n",nh[1][2][2]);
+  if(mympi.ThisTask == 0) printf("nh %f\n",nh[1][2][2]);
   ngamma = allocate_fftw_real_3d(N1,N2,N3+2);
+  if(mympi.ThisTask == 0) printf(" gamma %f\n",ngamma[1][2][2]);
   nxion=(fftw_real****)malloc(sizeof(fftw_real***)*Nnion);
   if(use_prev_xfrac == 1)
     xfrac=(fftw_real****)malloc(sizeof(fftw_real***)*Nnion);
@@ -172,7 +173,7 @@ int main(int argc, char **argv) {
     if(use_prev_xfrac == 1)
       xfrac[jk] = allocate_fftw_real_3d(N1,N2,N3+2);
   }
-
+  if(mympi.ThisTask == 0) printf("xion %f\n",nxion[0][1][2][2]);
   t_start =Get_Current_time();
 
   /* Allocate buffer to store 3D array */
