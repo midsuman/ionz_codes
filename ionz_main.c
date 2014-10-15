00f9a33c9a87f0b8791faf47d2fdb5b751eb4f19
boywert <boywert@Chaichalits-MacBook-Pro.local>
2014-10-15 16:07:56 +0100

    quick commit

--- ionz_main.c
+++ ionz_main.c
@@ -199,11 +199,9 @@ int main(int argc, char **argv) {
   if(mympi.ThisTask == 0) 
     debug_checkpoint();
 #endif
-
 #ifdef PARALLEL
   MPI_Barrier(MPI_COMM_WORLD);
 #endif
-  exit(1);
   if(mympi.ThisTask == 0) debug_checkpoint();
   if(mympi.ThisTask == 0) unpack_3d_array_mpi_transfer(buffer,nh,N1,N2,N3);
   if(mympi.ThisTask == 0) debug_checkpoint();
